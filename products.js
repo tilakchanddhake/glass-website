9e8e5551d29a6f4890dedbcb637aabb2e42ed45d
tilakchanddhake <113609322+tilakchanddhake@users.noreply.github.com>
2024-04-04 13:26:20 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,218 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-content");
+
+function opentab(tabname) {
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+            
+
+
+function cart() {
+    var rows="";
+    var title= document.getElementById("title").innerText;
+    var text= document.getElementById("text").innerText;
+    var price= document.getElementById("price").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart0() {
+    var rows="";
+    var title= document.getElementById("title0").innerText;
+    var text= document.getElementById("text0").innerText;
+    var price= document.getElementById("price0").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");  
+}
+
+function cart1() {
+    var rows="";
+    var title= document.getElementById("title1").innerText;
+    var text= document.getElementById("text1").innerText;
+    var price= document.getElementById("price1").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart2() {
+    var rows="";
+    var title= document.getElementById("title2").innerText;
+    var text= document.getElementById("text2").innerText;
+    var price= document.getElementById("price2").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart3() {
+    var rows="";
+    var title= document.getElementById("title3").innerText;
+    var text= document.getElementById("text3").innerText;
+    var price= document.getElementById("price3").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart4() {
+    var rows="";
+    var title= document.getElementById("title4").innerText;
+    var text= document.getElementById("text4").innerText;
+    var price= document.getElementById("price4").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart5() {
+    var rows="";
+    var title= document.getElementById("title5").innerText;
+    var text= document.getElementById("text5").innerText;
+    var price= document.getElementById("price5").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart6() {
+    var rows="";
+    var title= document.getElementById("title6").innerText;
+    var text= document.getElementById("text6").innerText;
+    var price= document.getElementById("price6").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart7() {
+    var rows="";
+    var title= document.getElementById("title7").innerText;
+    var text= document.getElementById("text7").innerText;
+    var price= document.getElementById("price7").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart8() {
+    var rows="";
+    var title= document.getElementById("title8").innerText;
+    var text= document.getElementById("text8").innerText;
+    var price= document.getElementById("price8").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart9() {
+    var rows="";
+    var title= document.getElementById("title9").innerText;
+    var text= document.getElementById("text9").innerText;
+    var price= document.getElementById("price9").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart10() {
+    var rows="";
+    var title= document.getElementById("title10").innerText;
+    var text= document.getElementById("text10").innerText;
+    var price= document.getElementById("price10").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart11() {
+    var rows="";
+    var title= document.getElementById("title11").innerText;
+    var text= document.getElementById("text11").innerText;
+    var price= document.getElementById("price11").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart12() {
+    var rows="";
+    var title= document.getElementById("title12").innerText;
+    var text= document.getElementById("text12").innerText;
+    var price= document.getElementById("price12").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart13() {
+    var rows="";
+    var title= document.getElementById("title13").innerText;
+    var text= document.getElementById("text13").innerText;
+    var price= document.getElementById("price13").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function submit(){
+    alert("You Successfully Completed Order.");
+}
+
+
+function cart14() {
+    var rows="";
+    var title= document.getElementById("title14").innerText;
+    var text= document.getElementById("text14").innerText;
+    var price= document.getElementById("price14").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart15() {
+    var rows="";
+    var title= document.getElementById("title15").innerText;
+    var text= document.getElementById("text15").innerText;
+    var price= document.getElementById("price15").innerText;
+    var quantity=1;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+function cart16() {
+    var rows="";
+    var title= document.getElementById("title16").innerText;
+    var text= document.getElementById("text16").innerText;
+    var price= document.getElementById("price16").innerText;
+    var quantity=1;
+    var total= price;
+    document.getElementsByTagName("tbody").innerHTML=rows+="<tr><td>"+title+"</td><td>"+text+"</td><td>"+price+"</td><td>"+quantity+"</td><td>"+total+"</td></tr>";
+    $(rows).appendTo("#table");
+}
+
+
